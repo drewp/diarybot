0ec4e09794e2215770b907c3cb297894430b5382
drewp <drewp@bigasterisk.com>
2012-07-11 20:59:17 -0700

    indexer
    Ignore-this: 647ba4c9e093dcd19ecf9d94e8cec9f0
    
    darcs-hash:20120712035917-312f9-40caa8ff05a279c622c21d5315bdbce94494126c.gz

--- buildIndex.py
+++ buildIndex.py
@@ -6,7 +6,7 @@ from __future__ import division
 import restkit, json, os, re
 from pymongo import Connection
 
-search = restkit.Resource("http://bang:8080/search_2.8.1-1.0.3/")
+search = restkit.Resource("http://bang:9096/")
 
 coll = Connection('bang', 27017)['diarybot']['aribot']
 for row in coll.find():
@@ -14,7 +14,7 @@ for row in coll.find():
     # todo
     uri = "http://bigasterisk.com/diarybot/%s/%s" % ('aribot', row['dc:created'])
     doc = dict(uri=uri,
-               title="Entry by %s at %s" % (row['dc:creator'],
+               title="Entry by %s at %s" % (label,
                                             row['dc:created']),
                text=txt)
     search.post("index", source="aribot", payload=json.dumps(doc))
