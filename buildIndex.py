3c4e3cef011884d9ebe06c102d534662aa6f5871
drewp <drewp@bigasterisk.com>
2012-07-11 21:00:26 -0700

    indexer prettier names
    Ignore-this: 23944f4ae6360ba4000d59ed779bf56d
    
    darcs-hash:20120712040026-312f9-d9c12ed9caa61a8af6855971f3164a39f16ae3ba.gz

--- buildIndex.py
+++ buildIndex.py
@@ -13,6 +13,12 @@ for row in coll.find():
     txt = row['sioc:content']
     # todo
     uri = "http://bigasterisk.com/diarybot/%s/%s" % ('aribot', row['dc:created'])
+
+    label = {
+        'http://bigasterisk.com/kelsi/foaf.rdf#kelsi' : 'Kelsi',
+        'http://bigasterisk.com/foaf.rdf#drewp' : 'Drew',
+        }.get(row['dc:creator'], row['dc:creator'])
+    
     doc = dict(uri=uri,
                title="Entry by %s at %s" % (label,
                                             row['dc:created']),
