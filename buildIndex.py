e314881bf84c68e59dcec86737c9668de2f63344
drewp <drewp@bigasterisk.com>
2019-07-29 10:05:42 -0700

    indexer to submit certain bot instances
    Ignore-this: 11016b0be3ca64072587357b81af7618
    
    darcs-hash:6188a646f9ae1c8934a5252ca531d385aa06cc04

--- buildIndex.py
+++ buildIndex.py
@@ -8,19 +8,20 @@ from pymongo import MongoClient as Connection
 
 search = restkit.Resource("http://bang:9096/")
 
-coll = Connection('bang', 27017)['diarybot']['aribot']
-for row in coll.find():
-    txt = row['sioc:content']
-    # todo
-    uri = "http://bigasterisk.com/diarybot/%s/%s" % ('aribot', row['dc:created'])
+for bot in ['aribot', 'asherbot']:
+    coll = Connection('bang', 27017)['diarybot'][bot]
+    for row in coll.find():
+        txt = row['sioc:content']
+        # todo
+        uri = "http://bigasterisk.com/diarybot/%s/%s" % (bot, row['dc:created'])
 
-    label = {
-        'http://bigasterisk.com/kelsi/foaf.rdf#kelsi' : 'Kelsi',
-        'http://bigasterisk.com/foaf.rdf#drewp' : 'Drew',
-        }.get(row['dc:creator'], row['dc:creator'])
-    
-    doc = dict(uri=uri,
-               title="Entry by %s at %s" % (label,
-                                            row['dc:created']),
-               text=txt)
-    search.post("index", source="aribot", payload=json.dumps(doc))
+        label = {
+            'http://bigasterisk.com/kelsi/foaf.rdf#kelsi' : 'Kelsi',
+            'http://bigasterisk.com/foaf.rdf#drewp' : 'Drew',
+            }.get(row['dc:creator'], row['dc:creator'])
+
+        doc = dict(uri=uri,
+                   title="Entry by %s at %s" % (label,
+                                                row['dc:created']),
+                   text=txt)
+        search.post("index", source=bot, payload=json.dumps(doc))
