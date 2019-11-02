a70bde143a14646c6bbfb8a3fc5a8e4de1a308b3
drewp <drewp@bigasterisk.com>
2019-11-01 22:53:06 -0700

    refactor queries
    Ignore-this: ba08df2adfeeab7af6f328ba72adaf77
    
    darcs-hash:b6bffd2370a33d6bc3068bc300968c74f2a141e5

--- diarybot2.py
+++ diarybot2.py
@@ -95,78 +95,7 @@ class StructuredInput(FixRequestHandler):
         self.write('saved')  # self.redirect(uri)
 
 
-class Query(object):
-    suffix = None
-
-    def makeLink(self, currentQuery):
-        levels = (currentQuery.suffix or '').count('/')
-        return './' + '../' * levels + 'history' + (self.suffix or '')
-
-    def makeHomeLink(self):
-        levels = (self.suffix or '').count('/')
-        return '../' * (levels + 1)
-
-
-class OffsetTime(Query):
-    def __init__(self, daysAgo, labelAgo, urlSuffix):
-        self.name = self.desc = labelAgo
-        self.daysAgo = daysAgo
-        self.suffix = urlSuffix
-
-    def run(self, mongo):
-        end = datetime.datetime.now() - datetime.timedelta(days=self.daysAgo)
-        rows = mongo.find({
-            'deleted': {
-                '$exists': False
-            },
-            'created': {
-                '$lt': end
-            }
-        }).sort('created', -1).limit(10)
-        rows = reversed(list(rows))
-        return rows
-
-
-class Last150(Query):
-    name = 'last 150 entries'
-    desc = name
-    suffix = '/recent'
-
-    def run(self, mongo):
-        return mongo.find({'deleted': {
-            '$exists': False
-        }},
-                          limit=150,
-                          sort=[('created', -1)])
-
-
-class Latest(Query):
-    name = 'latest entry'
-    desc = name
-    suffix = '/latest'
-
-    def run(self, mongo):
-        return mongo.find({'deleted': {
-            '$exists': False
-        }},
-                          limit=1,
-                          sort=[('created', -1)])
-
-
-class All(Query):
-    name = 'all'
-    desc = 'history'
-    suffix = None
-
-    def run(self, mongo):
-        return mongo.find({'deleted': {'$exists': False}}).sort('created', -1)
-
-
-def uriForDoc(botName, d):
-    return URIRef('http://bigasterisk.com/diary/%s/%s' % (botName, d['_id']))
-
-
-def getDoc(bot, agent):
+def getDoc(bot, agent, docId):
     if agent not in bot.owners:
         raise ValueError('not owner')
     return bot.mongo.find_one({'_id': ObjectId(docId)})  # including deleted
