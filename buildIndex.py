94dddf30e2f73b147fc64feb1f4f97c875c3ed32
drewp <drewp@bigasterisk.com>
2020-02-10 00:09:41 -0800

    quieter buildIndex logging
    Ignore-this: b0e489be604b7523ff9cfb764d47ee27
    
    darcs-hash:01b650c826acbf01ed3aab0b1034698f571cc17a

--- buildIndex.py
+++ buildIndex.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 """send diarybot entries to search."""
+import logging
 import requests
 import json
 from pymongo import MongoClient as Connection
 from chatinterface import ChatInterface
 from rdflib import Graph
 from bot import makeBots
+
+logging.basicConfig(level=logging.WARN)
+log = logging.getLogger()
+log.setLevel(logging.WARN)
 
 configGraph = Graph()
 configGraph.parse('bots-secret.n3', format='n3')
@@ -16,20 +21,23 @@ bots = makeBots(chat, configGraph)
 for botName in ['aribot', 'asherbot']:
     coll = Connection('bang', 27017)['diarybot'][botName]
     for row in coll.find():
-        txt = row['sioc:content']
-        uri = bots[botName].uriForDoc(row)
+        try:
+            txt = row['sioc:content']
+            uri = bots[botName].uriForDoc(row)
 
-        label = {
-            'http://bigasterisk.com/kelsi/foaf.rdf#kelsi': 'Kelsi',
-            'http://bigasterisk.com/foaf.rdf#drewp': 'Drew',
-        }.get(row['dc:creator'], row['dc:creator'])
+            label = {
+                'http://bigasterisk.com/kelsi/foaf.rdf#kelsi': 'Kelsi',
+                'http://bigasterisk.com/foaf.rdf#drewp': 'Drew',
+            }.get(row['dc:creator'], row['dc:creator'])
 
-        doc = dict(uri=uri,
-                   title='%s entry by %s at %s' % (
-                       botName, label, row['dc:created']),
-                   text=txt)
-        requests.post(
-            'http://bang:9096/index',
-            params={
-                'source': botName},
-            data=json.dumps(doc)).raise_for_status()
+            doc = dict(uri=uri,
+                       title='%s entry by %s at %s' % (
+                           botName, label, row['dc:created']),
+                       text=txt)
+            requests.post(
+                'http://bang:9096/index',
+                params={
+                    'source': botName},
+                data=json.dumps(doc)).raise_for_status()
+        except KeyError as e:
+            log.warn('%s: %r', row['_id'], e)
